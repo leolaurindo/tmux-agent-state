bd54c1fa069d637bf0065d2d64a6e643a84bcf8e
Leonardo Laurindo <leolaurindorj@gmail.com>
2026-08-10 21:47:05 -0300

    Support independent optional styles for running and idle states

--- tmux/agent-status.sh
+++ tmux/agent-status.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-highlight='#{?@agent_window,#{?#{==:#{@agent_highlight_when},idle},#{?@agent_running,,#[#{E:@agent_highlight_style}]},#{?@agent_running,#[#{E:@agent_highlight_style}],}},}'
+highlight='#{?@agent_window,#{?@agent_running,#{?@agent_running_style,#[#{E:@agent_running_style}],},#{?@agent_idle_style,#[#{E:@agent_idle_style}],}},}'
 
 for option in window-status-format window-status-current-format; do
 	format=$(tmux show-option -gv "$option")
+
 	case "$format" in
-		*@agent_running*) continue ;;
+		*@agent_running_style*|*@agent_idle_style*) continue ;;
 	esac
 
 	format=${format/\#W/$highlight#W}
